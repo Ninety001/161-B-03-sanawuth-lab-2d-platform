e279568406a0cd13aa2a7123ab9aa8a9b9aea888
sanawuth <sanawuth.homc@bumail.net>
2025-10-14 11:00:55 +0700

    TakeDamage,IsDead (Add inheritance)

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    private int health;
+    public int Health 
+    { get => health;
+      set => health = (value < 0) ? 0 : value; }
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        Debug.Log($"{this.name}:Take {damage}. HP left : {Health}");
+    }
+
+    public bool IsDead()
+    {
+        if (Health <= 0)
+        {
+            Debug.Log($"{this.name} is dead! and destroyed!");
+            Destroy(this.gameObject);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }    
+}
